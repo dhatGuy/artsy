855f7ec94aa01850abaa2f2fa58b2bdfc896d4ea
Joseph Odunsi <odunsiolakunbi@gmail.com>
2023-04-14 11:41:00 +0100

    setup for shadcn/ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        serif: ["var(--font-bellefair)", ...fontFamily.sans],
+        sans: ["var(--font-satoshi)", ...fontFamily.serif],
+        "clash-display": ["var(--font-clash-display)", ...fontFamily.sans],
+        "stix-two": ["var(--font-stix-two)", ...fontFamily.sans],
+        baskervville: ["var(--font-baskervville)", ...fontFamily.sans],
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("tailwindcss-animate")],
+};
